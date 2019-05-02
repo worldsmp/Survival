bd82dd758af96514f1eaedef9bd6ba43a32f756c
Spacey <finjsawyer@gmail.com>
2019-05-02 17:09:15 +0100

    Add brick recipe

--- src/main/java/com/theworldsmp/server/Survival.java
+++ src/main/java/com/theworldsmp/server/Survival.java
@@ -4,28 +4,33 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Survival extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
-		loadRecipe();
+		loadRecipes();
 	}
 
 	@Override
 	public void onDisable() {
 	}
 
-	private void loadRecipe() {
-		final ItemStack item  = new ItemStack(Material.QUARTZ_BLOCK, 4);
-		final NamespacedKey quartzkey = new NamespacedKey(this, "quartz_key");
-		final ShapedRecipe quartzrecipe = new ShapedRecipe(quartzkey, item);
+	private void loadRecipes() {
+		final ItemStack quartzItem  = new ItemStack(Material.QUARTZ_BLOCK, 4);
+		final NamespacedKey quartzKey = new NamespacedKey(this, "quartz_key");
+		final ShapelessRecipe quartzRecipe = new ShapelessRecipe(quartzKey, quartzItem);
 
-		quartzrecipe.shape("qq ","qq ","   ");
-		quartzrecipe.setIngredient('q', Material.QUARTZ);
+		quartzRecipe.addIngredient(4, Material.QUARTZ);
+		Bukkit.addRecipe(quartzRecipe);
 
-		Bukkit.addRecipe(quartzrecipe);
+		final ItemStack brickItem  = new ItemStack(Material.BRICKS, 4);
+		final NamespacedKey brickKey = new NamespacedKey(this, "brick_key");
+		final ShapelessRecipe brickRecipe = new ShapelessRecipe(brickKey, brickItem);
+
+		brickRecipe.addIngredient(4, Material.BRICK);
+		Bukkit.addRecipe(brickRecipe);
 	}
 }
